15b8efa7c5df165820f522db78eb042c3c29f2d0
olabee100 <olabayoantonio@gmail.com>
2023-01-19 16:24:53 +0530

    ok

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for char in range(26):
-    if char != 5 and char != 17:
-        print("{:s}".format(chr(char + ord("a"))), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) == 'e' or chr(i) == 'q':
+        continue
+    else:
+        print("{:s}".format(chr(i)), end="")
